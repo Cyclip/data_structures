1994157e3e0326cb82ce7dd0c48033b347ad4c16
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-06-22 20:42:55 +0100

    Add remove_child

--- rust/ds_tree/src/tree.rs
+++ rust/ds_tree/src/tree.rs
@@ -30,12 +30,36 @@ impl<T> Node<T> {
     }
 
     // Method to add a child to this Node instance
-    pub fn add_child(&self, child: Node<T>) {
+    pub fn add_child(&self, mut child: Node<T>) {
         // Only add if the new child instance isn't already a child
         if !self.has_child(child) {
             // Set relations
             child.parent = Some(Box::new(*self));
             self.children.push(Box::new(child));
         }
+    }
+
+    // Method to remove a child from this Node instance
+    pub fn remove_child(&self, child: Node<T>) {
+        // Remove their parent identifier
+        child.parent = None;
+
+        // Iterate and remove the child instance
+        self.children.remove(
+            self.children.iter().position(
+                /* Closure taking a pointer to the element
+                 * and checking if it matches a condition.
+                */
+                |&x| self.compare_nodes(*x, child)
+            ).unwrap()
+        );
+    }
+
+    pub fn has_child(&self, child: Node<T>) -> bool {
+        false
+    }
+
+    fn compare_nodes(&self, a: Node<T>, b: Node<T>) -> bool {
+        &a as *const _ == &b as *const _
     }
 }
