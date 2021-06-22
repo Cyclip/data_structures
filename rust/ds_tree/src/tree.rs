ec85c578781e7a1e9332450fec27dff1690a5afb
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-06-22 20:31:41 +0100

    Add add_child function

--- rust/ds_tree/src/tree.rs
+++ rust/ds_tree/src/tree.rs
@@ -1,14 +1,41 @@
 // Tree data structure implementation for Rust
 
+#[derive(PartialEq, PartialOrd)]
 pub struct Node<T> {
-    // Structure representing a node and it's children
-    pub data: T
+    /*
+     * Structure representing a node and its children/parents
+     * It's recommended to not directly change these variables,
+     * but use the methods implemented in the struct.
+     *
+     * Derives PartialEq & PartialOrd for comparing
+     */
+    pub data: T,
+    pub parent: Option<Box<Node<T>>>,
+    pub children: Vec<Box<Node<T>>>,
+
+    root: bool,
 }
 
 impl<T> Node<T> {
     // Static method to create a new instance of Node
     pub fn new(data: T) -> Node<T> {
-        let mut new = Node {data};
+        let mut new = Node {
+            data,
+            parent: None,
+            children: Vec::new(),
+            root: false,
+        };
+
         return new;
+    }
+
+    // Method to add a child to this Node instance
+    pub fn add_child(&self, child: Node<T>) {
+        // Only add if the new child instance isn't already a child
+        if !self.has_child(child) {
+            // Set relations
+            child.parent = Some(Box::new(*self));
+            self.children.push(Box::new(child));
+        }
     }
 }
