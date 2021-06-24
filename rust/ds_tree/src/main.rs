559337afd37dc55c33b076247888ff3d671fa816
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-06-24 22:32:53 +0100

    Change Node structure (Rust)

--- rust/ds_tree/src/main.rs
+++ rust/ds_tree/src/main.rs
@@ -1,6 +1,9 @@
 mod tree;
 
 fn main() {
-    let mut test = tree::Node::new(Strimg::from("testnode"));
-    println!("{:?}", test.relations.get("owner"));
+    let mut test = tree::Node::new(String::from("testnode"));
+    let mut test2 = tree::Node::new(String::from("testnode2"));
+    
+    // Make test2 a child of test
+    // test.add_child(&mut test2);
 }
--- rust/ds_tree/src/tree.rs
+++ rust/ds_tree/src/tree.rs
@@ -1,48 +1,26 @@
 // Tree data structure implementation for Rust
-use std::collections::HashMap;  // For structs
 
-/* 
- * It will use a hashmap which will be
- * created after initialization
- */
-
- #[derive(Debug)] // printing debug information
+#[derive(Debug)] // printing debug information
 pub struct Node<'a> {
     // Structure representing a node
     pub id: String,
-    pub relations: HashMap<
-        &'a str,
-        RelationValue<'a>
-    >,
+    pub parent: Option<&'a Node<'a>>,
+    pub children: Vec<&'a Node<'a>>,
 }
 
 impl<'a> Node<'a> {
     // Static method to create a new node
     pub fn new(id: String) -> Node<'a> {
-        let mut tmp_node = Node {
-            relations: HashMap::new(),
-            id: String::from(id),
-        };
-
-        tmp_node.relations.insert("owner", RelationValue::Single(None));
-        tmp_node.relations.insert("children", RelationValue::Vector(vec![])); // change to vector
-
-        tmp_node
+        Node {
+            id,
+            parent: None,
+            children: Vec::new(),
+        }
     }
 
     // Add a child to this node
-    pub fn add_child(&'a self, child: &mut Node<'a>) {
-        // Set the child's parent to this node
-        *child.relations.get_mut("owner").unwrap() = RelationValue::Single(Some(self));
+    pub fn add_child(&'a mut self, child: &'a mut Node<'a>) {
+        self.children.push(child);
+        child.parent = Some(self);
     }
-}
-
-#[derive(Debug)] // printing owner and children debug
-pub enum RelationValue<'a> {
-    Single(
-        Option<&'a Node<'a>>
-    ),
-    Vector(
-        Vec<&'a Node<'a>>
-    )
 }
