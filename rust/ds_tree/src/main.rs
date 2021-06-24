f963cd3114bc3dbbc490f705b04145ed9ac139be
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-06-24 15:55:34 +0100

    Add add_child function
    Setting the parents only

--- rust/ds_tree/src/main.rs
+++ rust/ds_tree/src/main.rs
@@ -1,5 +1,6 @@
 mod tree;
 
 fn main() {
-    let mut test = tree::Node::new();
+    let mut test = tree::Node::new(Strimg::from("testnode"));
+    println!("{:?}", test.relations.get("owner"));
 }
--- rust/ds_tree/src/tree.rs
+++ rust/ds_tree/src/tree.rs
@@ -1,12 +1,48 @@
 // Tree data structure implementation for Rust
+use std::collections::HashMap;  // For structs
 
-pub struct Node {
+/* 
+ * It will use a hashmap which will be
+ * created after initialization
+ */
+
+ #[derive(Debug)] // printing debug information
+pub struct Node<'a> {
     // Structure representing a node
+    pub id: String,
+    pub relations: HashMap<
+        &'a str,
+        RelationValue<'a>
+    >,
 }
 
-impl Node {
+impl<'a> Node<'a> {
     // Static method to create a new node
-    pub fn new() -> Node {
-        return Node {};
+    pub fn new(id: String) -> Node<'a> {
+        let mut tmp_node = Node {
+            relations: HashMap::new(),
+            id: String::from(id),
+        };
+
+        tmp_node.relations.insert("owner", RelationValue::Single(None));
+        tmp_node.relations.insert("children", RelationValue::Vector(vec![])); // change to vector
+
+        tmp_node
     }
+
+    // Add a child to this node
+    pub fn add_child(&'a self, child: &mut Node<'a>) {
+        // Set the child's parent to this node
+        *child.relations.get_mut("owner").unwrap() = RelationValue::Single(Some(self));
+    }
+}
+
+#[derive(Debug)] // printing owner and children debug
+pub enum RelationValue<'a> {
+    Single(
+        Option<&'a Node<'a>>
+    ),
+    Vector(
+        Vec<&'a Node<'a>>
+    )
 }
